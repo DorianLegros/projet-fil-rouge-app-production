e482ca1a6917b664ea8ad69dd8e3919d1728a20c
Dorian Legros <dorian.legros@outlook.fr>
2022-04-05 15:48:10 +0200

    initialized project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetFilBleu_AppProduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, c'est le projet fil rouge !");
+        }
+    }
+}
