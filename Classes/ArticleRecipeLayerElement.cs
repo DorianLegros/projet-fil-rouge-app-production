b4a0a4f1cba0b9d23592f1c2588ed2f82bfc5c45
Dorian Legros <dorian.legros@outlook.fr>
2022-06-28 10:40:19 +0200

    created recipe layers creation method

---
+++ Classes/ArticleRecipeLayerElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetFilBleu_AppProduction.Classes
+{
+    class ArticleRecipeLayerElement
+    {
+        public int IdFirstComponent { get; set; }
+        public int QuantityFirstComponent { get; set; }
+        public int IdSecondComponent { get; set; }
+        public int QuantitySecondComponent { get; set; }
+        public int IdOperation { get; set; }
+        public bool Done { get; set; } = false;
+    }
+}
---
+++ Functions/AlgorithmFunctions.cs
@@ -0,0 +1,42 @@
+﻿using ProjetFilBleu_AppProduction.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetFilBleu_AppProduction.Functions
+{
+    class AlgorithmFunctions
+    {
+        public Dictionary<int, List<ArticleRecipeLayerElement>> AddLayerRecipes(ArticleProductionTreeElement articleProductionTreeElement, Dictionary<int, List<ArticleRecipeLayerElement>> recipeLayersDic, int layer = 1)
+        {
+            try
+            {
+                if (articleProductionTreeElement.Recipe == null)
+                    return recipeLayersDic;
+
+                if (!recipeLayersDic.ContainsKey(layer))
+                    recipeLayersDic.Add(layer, new List<ArticleRecipeLayerElement>());
+
+                ArticleRecipeLayerElement layerEl = new ArticleRecipeLayerElement { IdFirstComponent = articleProductionTreeElement.Recipe.FirstComponent.Id, QuantityFirstComponent = articleProductionTreeElement.Recipe.FirstComponent.Quantity, IdOperation = (int)articleProductionTreeElement.Recipe.OperationId };
+
+                if (articleProductionTreeElement.Recipe.SecondComponent != null)
+                {
+                    layerEl.IdSecondComponent = articleProductionTreeElement.Recipe.SecondComponent.Id;
+                    layerEl.QuantitySecondComponent = articleProductionTreeElement.Recipe.SecondComponent.Quantity;
+                }
+
+                recipeLayersDic[layer].Add(layerEl);
+
+                recipeLayersDic = AddLayerRecipes(articleProductionTreeElement.Recipe.FirstComponent, recipeLayersDic, layer + 1);
+                if (articleProductionTreeElement.Recipe.SecondComponent != null)
+                    recipeLayersDic = AddLayerRecipes(articleProductionTreeElement.Recipe.SecondComponent, recipeLayersDic, layer + 1);
+
+                return recipeLayersDic;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
